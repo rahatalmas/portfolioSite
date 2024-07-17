1ad518ba801a4a635d714ce98772d62e7c9d2d1f
rahatalmas <rahat.almas5599@gmail.com>
2024-07-18 04:56:12 +0600

    thesis

--- src/components/ThesisCard.js
+++ src/components/ThesisCard.js
@@ -1,10 +1,26 @@
 import './ThesisCard.css';
 
-const ThesisCard = ({ThesisTitle,Abstract,KeyContribution,skills,link}) =>{
+const ThesisCard = ({thesis,link}) =>{
     return(
         <div className='thesisCard-container'>
-           <h1 className='thesis-title'>Title: {ThesisTitle}</h1>
-           <p className='abstract'>{Abstract}</p>
+           <h1 className='thesis-title'>Title: {thesis.thesisTitle}</h1>
+           <p className='abstract'>{thesis.abstract}</p>
+           <h3>Key Contributions</h3>
+           <ul>
+            <li>VisionTransformerAccuracy: {thesis.keyContributions.visionTransformerAccuracy}</li>
+            <li>cnnAccuracy: {thesis.keyContributions.cnnAccuracy}</li>
+            <li>{thesis.keyContributions.impact}</li>
+           </ul>
+           <h3>Skills Gained</h3>
+           <p>Technical Skills: {thesis.skillsGained.technicalSkills.map(skill=>(
+             <span>{skill}, </span>
+           ))}</p>
+           <p>Research Skills: {thesis.skillsGained.researchSkills.map(skill=>(
+             <span>{skill}, </span>
+           ))}</p>
+          <p>Technical Skills: {thesis.skillsGained.projectManagement.map(skill=>(
+             <span>{skill}, </span>
+           ))}</p>
         </div>
     );
 }
--- src/pages/ResearchPage.js
+++ src/pages/ResearchPage.js
@@ -3,13 +3,28 @@ import ThesisCard from '../components/ThesisCard';
 import './ResearchPage.css';
 
 const ResearchPage = () =>{
+
+    const thesisDetails = {
+        thesisTitle: "Salinity-Resilient Crop Health Monitoring: Automated Disease Detection in Luffa Aegyptiaca Leaves using Vision Transformer & CNN",
+        abstract: "Leveraged advanced deep learning techniques to automate disease detection in Luffa Aegyptiaca leaves. Collected and categorized images of leaves infected with Cucumber Mosaic Virus, Downey Mildew, and Leaf Spot. Achieved high accuracy with both CNN and Vision Transformer models.",
+        keyContributions: {
+          visionTransformerAccuracy: "99.85%",
+          cnnAccuracy: "98.32%",
+          impact: "Demonstrated the potential of deep learning models in automating disease detection in agricultural crops, particularly in saline-affected coastal regions."
+        },
+        skillsGained: {
+          technicalSkills: ["Vision Transformer", "Convolutional Neural Network (CNN)", "Google Colab"],
+          researchSkills: ["Image collection and categorization", "Model training and evaluation"],
+          projectManagement: ["Data management", "Cloud computing", "Collaborative research"]
+        }
+      };
+
     return(
         <div className='page-container'>
            <IndexHeader title1="Thesis"/>
            <div className='thesis-container'>
               <ThesisCard 
-              ThesisTitle={"Salinity-Resilient Crop Health Monitoring: Automated Disease Detection in Luffa Aegyptiaca Leaves using Vision Transformer & CNN"}
-              Abstract={"Leveraged advanced deep learning techniques to automate disease detection in Luffa Aegyptiaca leaves. Collected and categorized images of leaves infected with Cucumber Mosaic Virus, Downey Mildew, and Leaf Spot. Achieved high accuracy with both CNN and Vision Transformer models."}
+              T thesis={thesisDetails}
               />
            </div>
 
